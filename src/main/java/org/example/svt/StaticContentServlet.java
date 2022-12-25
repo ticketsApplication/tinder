a2853c86ff10c012af207ca9eb38a85320772653
BukanNadya <118114796+BukanNadya@users.noreply.github.com>
2022-12-25 12:30:15 +0200

    Tinger servlet (#2)

---
+++ src/main/java/org/example/svt/StaticContentServlet.java
@@ -0,0 +1,30 @@
+package org.example.svt;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class StaticContentServlet extends HttpServlet {
+
+    private final String osPrefix;
+    public StaticContentServlet(String osPrefix) {
+        this.osPrefix = osPrefix;
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getPathInfo();
+
+        if (pathInfo.startsWith("/")) pathInfo = pathInfo.substring(1);
+
+        Path file = Path.of(osPrefix, pathInfo);
+
+        try (ServletOutputStream os = resp.getOutputStream()) {
+            Files.copy(file, os);
+        }
+    }
+}
