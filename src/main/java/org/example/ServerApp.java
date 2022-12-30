05a9ae1a1a58093878c582a5d95a6a14eabf2c2e
BukanNadya <118114796+BukanNadya@users.noreply.github.com>
2022-12-30 21:44:40 +0200

    Login servlet (#11)
    * Login Servlet
    
    * Merge branch 'main' into loginServlet
    
    # Conflicts:
    #	src/main/java/org/example/tinderDAO/CollectionTinderDao.java
    #	static-content/html/login.html
    
    LoginServlet, SetCookieServlet, RemoveCookieServlet
    
    * LoginServlet, SetCookieServlet, RemoveCookieServlet

--- src/main/java/org/example/ServerApp.java
+++ src/main/java/org/example/ServerApp.java
@@ -12,6 +12,8 @@ import org.example.tinderDAO.CollectionTinderDao;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 
 public class ServerApp {
@@ -20,10 +22,12 @@ public class ServerApp {
     //  http://localhost:8080/users
     //  http://localhost:8080/liked
     //  http://localhost:8080/login
+    //  http://localhost:8080/setcookie
+    //  http://localhost:8080/removecookie
+    //  http://localhost:8080/signin
     //  http://localhost:8080/chat
     //  http://localhost:8080/dynamicusers
     public static void main(String[] args) throws Exception {
-
 
         CollectionTinderDao collectionTinderDao = new CollectionTinderDao();
 
@@ -34,9 +38,16 @@ public class ServerApp {
         Server server = new Server(8080);
         ServletContextHandler handler = new ServletContextHandler();
 
+        LoginServlet loginServlet = new LoginServlet(collectionTinderDao, conf);   ///////////////////////////
+        LoginHistoryServlet loginHistoryServlet = new LoginHistoryServlet(collectionTinderDao, conf);   /////////////////
+
         handler.addServlet(new ServletHolder(new UsersServlet(collectionTinderDao, conf)), "/users");
         handler.addServlet(new ServletHolder(new PeopleListServlet(collectionTinderDao, conf)), "/liked");
-        handler.addServlet(LoginServlet.class, "/login");
+        handler.addServlet(new ServletHolder(loginServlet), "/login");   /////////
+        handler.addServlet(new ServletHolder(loginHistoryServlet), "/loginHistory");   ////////////
+        handler.addServlet(SignInServlet.class, "/signin");
+        handler.addServlet(SetCookieServlet.class, "/setcookie");
+        handler.addServlet(RemoveCookieServlet.class, "/removecookie");
 
         handler.addServlet(ChatServlet.class, "/messages/{id}");
 
