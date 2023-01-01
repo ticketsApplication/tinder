13fa1ba6764b5308468d26eb02e562c8c67f5ccf
Andrii Horbenko <andrewhorbenko@gmail.com>
2023-01-01 17:14:01 +0200

    second commit

--- src/main/java/org/example/ServerApp.java
+++ src/main/java/org/example/ServerApp.java
@@ -10,18 +10,20 @@ import org.example.svt.*;
 import org.example.tinderDAO.CollectionTinderDao;
 
 
+import javax.servlet.DispatcherType;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.EnumSet;
 
 
 public class ServerApp {
 
-
+    private static final EnumSet<DispatcherType> ft = EnumSet.of(DispatcherType.REQUEST);
     //  http://localhost:8080/users
     //  http://localhost:8080/liked
     //  http://localhost:8080/login
     //  http://localhost:8080/chat
-    //  http://localhost:8080/dynamicusers
+
     public static void main(String[] args) throws Exception {
 
 
@@ -37,11 +39,11 @@ public class ServerApp {
         handler.addServlet(new ServletHolder(new UsersServlet(collectionTinderDao, conf)), "/users");
         handler.addServlet(new ServletHolder(new PeopleListServlet(collectionTinderDao, conf)), "/liked");
         handler.addServlet(LoginServlet.class, "/login");
-
-        handler.addServlet(ChatServlet.class, "/messages/{id}");
-
-
+        handler.addServlet(new ServletHolder(new ChatServlet(collectionTinderDao, conf)), "/messages/*");
         handler.addServlet(new ServletHolder(new StaticContentServlet("static-content")), "/static/*");
+
+
+        handler.addFilter(CheckCookiesServlet.class, "/users", ft);
 
         server.setHandler(handler);
 
